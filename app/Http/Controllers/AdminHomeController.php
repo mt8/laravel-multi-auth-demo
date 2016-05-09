ff50351ab135b8cebb80aab86c793736fff3eee3
mt8 <info@mt8.biz>
2016-05-09 23:05:20 +0900

    edit AdminHomeController.php

--- app/Http/Controllers/AdminHomeController.php
+++ app/Http/Controllers/AdminHomeController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+use Illuminate\Http\Request;
 
 class AdminHomeController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:webadmin');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('admin.home');
+    }
 }
